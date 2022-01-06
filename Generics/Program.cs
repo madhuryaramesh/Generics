b929f65c0d1c0815eda1069ed19260aa3bcb9bcd
madhurya <madhuryar017@gmail.com>
2022-01-06 12:55:36 +0530

    Initial commit

---
+++ Generics/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Generics
+{
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            Console.WriteLine("THE GENERICS PROGRAM ");
+
+          
+
+        }
+    }
+}
